44ec8e18b5ad54879ef459c0adad49573c1874b9
jhon gracia <jhon gracia@jhon>
2016-01-20 18:28:09 -0500

    commit

---
+++ IS2TEST/src/main/java/com/mycompany/is2test/Error.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.is2test;
+
+/**
+ *
+ * @author jhon gracia
+ */
+public class Error {
+    baia biai=o;
+    
+}
